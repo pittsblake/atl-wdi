a03419efce846245d93b0271cf002a14a36e7849
pittsblake <pittsblake1@gmail.com>
2017-09-06 21:20:21 -0400

    progress-bar  update

--- homework/javaScript/progress-bar/hw.js
+++ homework/javaScript/progress-bar/hw.js
@@ -4,15 +4,30 @@
 
 var timerUI = {
   drawNumericDisplay: function(timerValue){
-    // Your Code Here
+    document.getElementById('numeric-display').textContent = timerValue;
   },
   drawProgressBars: function(timerValue){
-    // Your Code Here
+    var timePassed = 100 - timerValue;
+    document.getElementsByClassName('progress-bar')[0].style.width = timePassed + '%';
   },
   drawLitFuses: function(timerValue){
-    // Your Code Here
+    const timePassed = 100 - timerValue;
+    document.getElementsByClassName('burnt')[0].style.width = timePassed * .98 + '%';
+    document.getElementsByClassName('unburnt')[0].style.width = timerValue * .98 + '%';
+    
   },
   drawCrawlers: function(timerValue){
-    // Your Code Here
+    const moveBug = 100 - timerValue;
+    
+    document.getElementsByClassName('crawler')[0].style.marginLeft = (moveBug * 10) + 'px';
+    
+    if (timerValue % 2 === 0) {
+      document.getElementsByClassName('crawler')[0].style.marginTop = 10 + 'px';
+    }
+    else{
+      document.getElementsByClassName('crawler')[0].style.marginTop = 0 + 'px';
+      
+    }
+    
   }
 };
