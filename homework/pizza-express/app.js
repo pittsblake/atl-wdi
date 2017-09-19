529d9b3899d83eaf1481bff83bfe4c74cf6423be
pittsblake <pittsblake1@gmail.com>
2017-09-19 13:38:42 -0400

    completed GET routes

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -5,6 +5,18 @@ const hbs = require('hbs');
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Pizza Express');
+});
+
+app.get('/topping/:type', (req, res) => {  
+    res.send(`${req.params.type} pizza!`);
+});
+
+app.get('/order/:amount/:size', (req, res) => {
+    res.send(`${req.params.amount} and ${req.params.size} on it's way!`);
+});
+
 
 
 const PORT = 3002;
