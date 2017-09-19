4f12d881c222c4ecabcac56d909c9a651f52bf11
pittsblake <pittsblake1@gmail.com>
2017-09-19 13:09:25 -0400

    in class exercises

--- hello-express/server.js
+++ hello-express/server.js
@@ -1,50 +1,33 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
-app.get('/', (request, response) => {
-    response.send("hello wdi12");
-})
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + '/public'));
 
+
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
 app.get('/greeting', (req, res) => {
-    res.send("Hey! WDI12!!");
-})
-
-app.get('/:name', (req, res) => {
-    console.log(req.params);
-    res.send(`Hello, ${req.params.name}`);
-})
-
-
-app.get('/food/:food', (req, res) => {
-    console.log(req.params);
-    res.send(`Wow, ${req.params.food} is great`);
-})
-
-app.get('/rihanna', (req, res) => {
-    res.send("work work work work");
-})
-
-app.get('/math/add', (req, res) => {
-    const num1 = parseInt(req.query.num1);
-    const num2 = parseInt(req.query.num2);
-    res.send(`${num1} + ${num2}`);
-})
-
-app.get('/math:operator', (req, res) => {
-    const num1 = req.query.num1;
-    const num2 = req.query.num2;
-    if(req.params.operator === "add"){
-        res.send(`${num1} + ${num2}`);
-    } else if (req.params.operator === "subtract"){
-        res.send(`${num1} - ${num2}`);
-    }c
-})
-
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log("Express is listening on port", PORT);
+    console.log(req.query)
+    res.render('greeting', {
+        data: req.query.saying,
+        name: "Blake",
+        height: `5'11`,
+    });
 });
 
+app.get('/favorite-foods', (req, res) => {
+    var favoriteFoods = ["Jeni's Almond Butter ice cream", 'Tacos from Superica', 'A Breakfast Sandwich from Gjelina to go in Venice', 'Croissants from Bottega Louie in Downtown Los Angeles', 'Pizza from Little Star in San Francisco'];
 
+
+    res.render('favorite-foods', {
+        favoriteFoods: favoriteFoods,
+    });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log("Express is listening on port ", PORT);
+})
