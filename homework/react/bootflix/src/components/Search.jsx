f5a7a2f25d91afeb2f0001e06ad9dc2c1f285acd
pittsblake <pittsblake1@gmail.com>
2017-10-10 19:55:48 -0400

    completed hw

--- homework/react/bootflix/src/components/Search.jsx
+++ homework/react/bootflix/src/components/Search.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 // Update the forms to utilize methods being passed down from App.js
-const Search = () => {
-  return (
-    <section id="movie-search">
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      id: ''
+    }
+  }
+
+  render() {
+  return(
+    <section id= "movie-search" >
       <strong>Search by:</strong> Title <em>or</em> ID
 
-      <div className="search">
-        <form id="title-search-form" method="get">
-          <input type="text" name="title" placeholder="Enter movie title" />
-          <input type="submit" value="Search for this title" />
-        </form>
+      <div className= "search" >
+      <form onSubmit={this.props.searchByTitle} id="title-search-form" method="get">
+        <input type="text" name="title" placeholder="Enter movie title" />
+        <input type="submit" value="Search for this title" />
+      </form>
       </div>
 
       <div className="search">
-        <form id="id-search-form" method="get">
+        <form onSubmit={this.props.searchById}id="id-search-form" method="get">
           <input type="text" name="id" placeholder="Enter omdb movie ID" />
           <input type="submit" value="Search by ID" />
         </form>
@@ -22,6 +33,7 @@ const Search = () => {
 
     </section>
   );
+}
 };
 
 export default Search;
