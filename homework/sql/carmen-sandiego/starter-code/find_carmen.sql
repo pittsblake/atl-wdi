6246a6301434db40dffff5b72e02edc75453aa63
pittsblake <pittsblake1@gmail.com>
2017-10-19 17:33:52 -0400

    halfway

---
+++ homework/sql/carmen-sandiego/starter-code/find_carmen.sql
@@ -0,0 +1,18 @@
+1 
+ 
+ SELECT name, region, population  FROM country WHERE region='Southern Europe' ORDER BY population;
+
+-- VATICAN CITY
+
+SELECT language FROM countrylanguage WHERE countrycode = 'VAT';
+
+-- ITALIAN
+
+SELECT * FROM countrylanguage WHERE language = 'Italian' ORDER BY percentage = 100;
+
+--SMR
+
+SELECT name FROM city WHERE countrycode = 'SMR';
+
+--SERAVAlle
+
