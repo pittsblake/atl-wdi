4df1567bb08b9769da5b2e1b202c2ca4dc6f4172
pittsblake <pittsblake1@gmail.com>
2017-10-19 17:50:55 -0400

    finished carmen san diego

--- homework/sql/carmen-sandiego/starter-code/find_carmen.sql
+++ homework/sql/carmen-sandiego/starter-code/find_carmen.sql
@@ -13,6 +13,14 @@ SELECT * FROM countrylanguage WHERE language = 'Italian' ORDER BY percentage = 1
 --SMR
 
 SELECT name FROM city WHERE countrycode = 'SMR';
+SELECT * FROM city WHERE name LIKE 'Serr%';
 
 --SERAVAlle
+--Brazil
 
+SELECT name FROM city WHERE id = '211'; 
+-- Brasï¿½lia
+
+SELECT name, district, countrycode FROM city WHERE population = 91084;
+
+--Santa Monica
