67601b1aa459bfde8f12d258b32b5d95db076424
pittsblake <pittsblake1@gmail.com>
2017-10-23 13:48:25 -0400

    2. Nill Skillz

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,23 +1,32 @@
-@a = true
-@b = false
+# @a = true
+# @b = false
 
 
-def test
-    if @b == false 
-    @a = false
-    puts @a
-    end
-end
+# def test
+#     if @b == false 
+#     @a = false
+#     puts @a
+#     end
+# end
 
-test
+# test
 
-def test2 
-    if @a == true 
-        @b = true
-        puts @b
-    end
-end
+# def test2 
+#     if @a == true 
+#         @b = true
+#         puts @b
+#     end
+# end
 
-test2
+# # test2
 
-:dart Commit -m "1. Boolean Skills"
+# # :dart Commit -m "1. Boolean Skills"
+
+
+# @x == nil
+#  print @x
+# puts "#{@x} was here"
+#     print @x
+# puts @x.class
+
+# :dart: Commit -m "2. Nil SKills"
